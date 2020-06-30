e0fcacc77cabe724bd2c9e5864a1750c1f7455a9
Jesam <61584105+theofficialBF@users.noreply.github.com>
2020-07-01 00:43:32 +0100

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SportsHandballIcon from "@material-ui/icons/SportsHandball";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <SportsHandballIcon />
+        Neuer App
+      </h1>
+      <p>You write your goals, he saves them.</p>
+    </header>
+  );
+}
+
+export default Header;
